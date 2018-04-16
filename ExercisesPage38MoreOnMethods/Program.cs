df4ab821aadfdb508ae05dcdafac01a4753407bd
ecallison <erinjccallison@yahoo.com>
2018-04-16 16:29:00 -0700

    I completed the exercises on page 38 regarding the "More on Methods" lecture.

---
+++ ExercisesPage38MoreOnMethods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesPage38MoreOnMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeeklyWageCalculator weeklyWageCalculator = new WeeklyWageCalculator();
+
+            Console.WriteLine("Hello Employees of Callison Tech! Please enter the number of hours you worked this week:");
+            Int32.TryParse(Console.ReadLine(), out int weeklyHours);
+
+            Console.WriteLine("\nThank you! Please enter a number that represents the amount you make per hour. If you do not enter your wage, we will assume you earn $55/hour :)");
+            bool yes = Int32.TryParse(Console.ReadLine(), out int employeeWage);
+            if (yes) 
+            {
+                int product = weeklyWageCalculator.CalculateWeeklyEarnings(weeklyHours, employeeWage);
+
+                Console.WriteLine("\nYou earned ${0}" + " this week!", product);
+            }
+            else
+            {
+                int product = weeklyWageCalculator.CalculateWeeklyEarnings(weeklyHours);
+
+                Console.WriteLine("\nYou earned ${0}" + " this week!", product);
+
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExercisesPage38MoreOnMethods/WeeklyWageCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesPage38MoreOnMethods
+{
+    class WeeklyWageCalculator
+    {
+        public int CalculateWeeklyEarnings(int weeklyHours, int employeeWage = 55)
+        {
+            return weeklyHours * employeeWage;
+        }
+
+        //public int CalculateWeeklyEarnings(int employeeWage, int timesMultiplied = 40)
+        //{
+        //    for(int i = 0; i < timesMultiplied; i++)
+        //    {
+        //        return empl
+        //    }
+        //}
+    }
+}
