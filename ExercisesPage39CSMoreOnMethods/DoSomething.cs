79d7d6b67b2f5514629c938ab9c9b28cb21978ab
ecallison <erinjccallison@yahoo.com>
2018-04-17 10:12:06 -0700

    I completed the exercises on page 39 regarding the lecture entitled "More on methods" in the new C# course.

---
+++ ExercisesPage39CSMoreOnMethods/DoSomething.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesPage39CSMoreOnMethods
+{
+    class DoSomething
+    {
+        public void CalculateWeeklyEarnings(int weeklyHours, int employeeWage)
+        {
+            int Answer1 = employeeWage * weeklyHours;
+
+            Console.WriteLine("You earn ${0} per hour.", employeeWage);
+
+            Console.WriteLine(" You earned ${0} this week.", Answer1);
+        }
+    }
+}
---
+++ ExercisesPage39CSMoreOnMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesPage39CSMoreOnMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoSomething doSomething = new DoSomething();
+            Console.WriteLine("Hello Employees of Callison Tech! Please enter the number of hours you worked this week:");
+            Int32.TryParse(Console.ReadLine(), out int weeklyHours);
+
+            Console.WriteLine("\nThank you! Please enter a number that represents the amount you make per hour:");
+            Int32.TryParse(Console.ReadLine(), out int employeeWage);
+
+            doSomething.CalculateWeeklyEarnings(weeklyHours, employeeWage);
+
+            Console.ReadLine();
+        }
+    }
+}
