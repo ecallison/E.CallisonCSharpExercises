badb4a310aca49d2077d2d55fbf6999b3627081f
ecallison <erinjccallison@yahoo.com>
2018-04-19 13:00:10 -0700

    I completed the exercises on page 43 regarding inheritance.

---
+++ ExercisesPage43CSIteration/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesPage43CSInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
