43273d2bbd62951b051500d22cc1ea7d823a302d
ecallison <erinjccallison@yahoo.com>
2018-04-21 19:54:53 -0700

    I completed the exercises on page 49 regarding interfaces.

---
+++ ExercisesPage49CSInterfaces/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesPage49CSInterfaces
+{
+    class Employee: IQuittable
+    {
+        public void Quittable()
+        {
+            Console.WriteLine("Your first and last name has been removed from your employee file. Goodbye.");
+        }
+    }
+}
---
+++ ExercisesPage49CSInterfaces/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesPage49CSInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable quittable = new Employee();
+            quittable.Quittable();
+            Console.ReadLine();
+        }
+    }
+}
