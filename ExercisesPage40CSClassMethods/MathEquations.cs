a2d279a6dc45e7aaa1a0cceb32979d66d1bb21c2
ecallison <erinjccallison@yahoo.com>
2018-04-21 18:17:42 -0700

    I deleted unnecessary commented out code.

--- ExercisesPage40CSClassMethods/MathEquations.cs
+++ ExercisesPage40CSClassMethods/MathEquations.cs
@@ -29,18 +29,3 @@ namespace ExercisesPage40CSClassMethods
     }
 }
 
-
-
-
-
-//1. Create a class. In that class, create a void method that outputs an integer.Have the method divide the data passed to it by 2.
-
-//2. In the Main() method, instantiate that class.
-
-//3. Have the user enter a number.Call the method on that number.Display the output to the screen. It should be the entered number, divided by two.
-
-//4. Create a method with output parameters.
-
-//5. Overload a method.
-
-//6. Declare a class to be static.
--- ExercisesPage47CS/Employee.cs
+++ ExercisesPage47CS/Employee.cs
@@ -15,4 +15,3 @@ namespace ExercisesPage47CS
         }
     }
 }
-
