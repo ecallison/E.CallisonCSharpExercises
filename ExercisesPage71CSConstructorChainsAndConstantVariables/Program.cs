8493bb4b236aca63101248072c2a623d2667b7de
ecallison <erinjccallison@yahoo.com>
2018-05-02 16:18:58 -0700

    I completed the exercises on page 71 regarding constant variables and constructor chains.

---
+++ ExercisesPage71CSConstructorChainsAndConstantVariables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesPage71CSConstructorChainsAndConstantVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercises Page 71 Constant Variables/Constructor Chains
+
+            //Exercise 1
+            const string myName = "Erin Callison";
+            Console.WriteLine("\n{0} is my name and I made it a constant variable :).", myName);
+           
+            //Exercise 2
+            Console.WriteLine("\nHello! Please type something so I can make a variable using the keyword \"var\" please :)!");
+            var yourInput = Console.ReadLine();
+            Console.WriteLine("\nYay! Thank you! You made \"{0}\" the variable.", yourInput);
+
+            //Exercise 3
+            Console.WriteLine("\nNow please enter your name:");
+            string yourName = Console.ReadLine();
+            Users users = new Users(yourName);
+            Console.WriteLine("\nHello {0}. We set your age to the default age of {1}.", yourName, users.Age);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExercisesPage71CSConstructorChainsAndConstantVariables/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesPage71CSConstructorChainsAndConstantVariables
+{
+    public class Users
+    {
+        public Users(string name) : this(name, 18)
+        {
+        }
+            
+        public Users(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+}
