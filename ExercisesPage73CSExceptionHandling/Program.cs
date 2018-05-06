f5b38fc34fa8597d1cb8fc9f36c61619f6ec3e61
ecallison <erinjccallison@yahoo.com>
2018-05-05 17:32:17 -0700

    I completed the exercises on page 73 regarding exception handling.

---
+++ ExercisesPage73CSExceptionHandling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesPage73CSExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter your age:");
+                int userAge = Convert.ToInt32(Console.ReadLine());
+                bool correctAge = userAge <= 0;
+                if (correctAge)
+                {
+                    throw new ZeroOrNegativeException();
+                }
+
+                int getYearBorn = 2018 - userAge;
+                Console.WriteLine("You were born in {0}.", getYearBorn);
+                Console.ReadLine();
+                return;
+            }
+            catch (ZeroOrNegativeException a)
+            {
+                Console.WriteLine("Please do not enter a negative number or a zero silly. Please enter your age in digits (with no decimals).");
+            }
+            catch (ArgumentException b)
+            {
+                Console.WriteLine("Please enter simply your age in digits. Please do not type, for example, \"18 years\".");
+            }
+            catch (Exception c)
+            {
+                Console.WriteLine("Please enter simply your age in digits (no negative numbers or decimals please). Please do not type, for example, \"18 years\".");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
